fb45fbc1c652343d8369daada7fdb16a4f799fef
Cristhianjvg <cristhianvillamarin2002@gmail.com>
2025-02-13 11:53:13 -0500

    cambiar route hello

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,3 +1,6 @@
+// app/api/hello/route.ts
+import { NextResponse } from "next/server";
+
 export async function GET(request: Request) {
-  return new Response("Hello, Next.js!");
+  return NextResponse.json({ message: "Hello, Next.js!" });
 }
