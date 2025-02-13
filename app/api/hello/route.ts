aff1c415b9ce69bb85141e438cf04343c346ae91
Cristhianjvg <cristhianvillamarin2002@gmail.com>
2025-02-13 11:38:01 -0500

    subir todo el proyecto pero especialemtne solo usar el pai

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,3 +1,5 @@
-export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+import { NowRequest, NowResponse } from "@vercel/node";
+
+export default async function handler(req: NowRequest, res: NowResponse) {
+  res.status(200).json({ message: "Hello from API" });
 }
